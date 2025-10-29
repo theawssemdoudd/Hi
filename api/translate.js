0ddf8076c4ce2ae3de9f527a0ee857cecb97fbd1
theawssemdoudd <143346166+theawssemdoudd@users.noreply.github.com>
2025-10-29 17:38:53 +0100

    requirements

---
+++ api/translate.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  const response = await fetch('https://libretranslate.de/translate', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(req.body),
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
